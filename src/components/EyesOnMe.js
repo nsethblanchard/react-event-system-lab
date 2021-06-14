bbcdbe896d79b6df6209f4def881d45d803779ca
Seth Blanchard <nsethblanchard@gmail.com>
2021-06-14 14:52:32 -0400

    commit tree 841c688a5640fefaccf4851d6dc35bf45f383735

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react'
+
+export class EyesOnMe extends Component {
+
+    focused = () => {
+        console.log('Good!')
+    }
+
+    blurred = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+           <button
+           onFocus={this.focused}
+           onBlur={this.blurred}
+           >
+
+
+           </button>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+export class Keypad extends Component {
+
+    selectedPhrase = () => {
+        console.log('Entering password...')
+    }
+
+
+    render() {
+        return (
+            <input type="password" onKeyUp={this.selectedPhrase}/>
+        )
+    }
+}
+
+export default Keypad
